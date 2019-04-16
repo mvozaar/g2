dad45e474278967507ff3c840f70704aaf958bb1
Martin Vozar <mvozaar@hotmail.com>
2019-04-16 02:21:16 +0200

    upon+once more

--- test/test.js
+++ test/test.js
@@ -2,7 +2,8 @@ const assert = require('assert');
 
 describe('my first', () => {
     it('is running', () => assert.equal(true,true));
-    it('once upan a time ... ', () => assert.ok(true));
+    it('once upon a time ... ', () => assert.ok(true));
     it('is running too', () => assert.equal(true,true));
+    it('is running once more time', () => assert.equal(true,true));
     it('last but running too', () => assert.equal(false,false));
 });
